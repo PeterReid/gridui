0154092ef536853de07eb7ea8980dacb5df16fb7
Peter Reid <peter.d.reid@gmail.com>
2015-02-14 21:50:59 -0500

    Cargofy

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+
+#![crate_type = "lib"]
+#![crate_name = "gridui"]
+
+//#[phase(plugin, link)]
+//extern crate log;
+
+#![feature(libc, std_misc)]
+
+extern crate libc;
+
+extern crate winapi;
+
+#[macro_use]
+extern crate "rust-windows" as windows;
+extern crate "gdi32-sys" as gdi32;
+extern crate "kernel32-sys" as kernel32;
+extern crate "user32-sys" as user32;
+
+pub mod gridui;
+
+
